273d21e8301afea7dbeb2243e2991a682f0fb3bd
Federico Muscarà <federicomuscara@libero.it>
2023-06-13 23:50:15 +0200

    First Upload

---
+++ app/src/main/java/com/timesup/HourglassBoundService.kt
@@ -0,0 +1,131 @@
+package com.timesup
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.CountDownTimer
+import android.os.IBinder
+import android.util.Log
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class HourglassBoundService : Service() {
+
+    private val CHANNEL_ID = "hourglass_channel"
+    private val NOTIFICATION_ID = 2
+    private val binder : IBinder = HourglassBinder()
+    private var countDownTimer: CountDownTimer? = null
+    private var remainingTime: Long = MainActivity.TIMER_DURATION
+    private var isFirstPortraitCall : Boolean = false
+    private var isFirstReverseCall : Boolean = true
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        remainingTime = getSharedPreferences("MyPreferences", MODE_PRIVATE).getLong("remainingTime", MainActivity.TIMER_DURATION)
+
+        val notificationBuilder: Notification.Builder =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                Notification.Builder(applicationContext, CHANNEL_ID)
+            else  // Deprecation warning left on purpose for educational reasons
+                Notification.Builder(applicationContext)
+
+        notificationBuilder.setContentTitle("Foreground")
+        notificationBuilder.setContentText("Time's Up! is running")
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    inner class HourglassBinder : Binder() {
+        val service: HourglassBoundService
+            get() = this@HourglassBoundService
+    }
+
+    fun setRemainingTime(time:Long){
+        remainingTime = time
+    }
+
+    fun setFirstPortraitCall(boolValue: Boolean){
+        isFirstPortraitCall = boolValue
+    }
+
+    fun setFirstReverseCall(boolValue: Boolean){
+        isFirstReverseCall = boolValue
+    }
+
+    fun startTimer(){
+        countDownTimer = object : CountDownTimer(remainingTime, MainActivity.COUNT_DOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) {
+                remainingTime = millisUntilFinished
+                Log.e("Service", "onTick $remainingTime")
+            }
+
+            override fun onFinish() {
+                remainingTime = MainActivity.TIMER_DURATION
+                //Generate notification: time's up!
+                buildNotification()
+            }
+        }.start()
+    }
+
+    fun stopTimer(){
+        countDownTimer?.cancel()
+    }
+
+    fun createNotificationChannel() {
+        // Verifying Android Version (>= 8)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notificationName)
+            val descriptionText = getString(R.string.descriptionText)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun buildNotification(){
+        val notificationBuilder: Notification.Builder =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                Notification.Builder(applicationContext, CHANNEL_ID)
+            else  // Deprecation warning left on purpose for educational reasons
+                Notification.Builder(applicationContext)
+
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
+        notificationBuilder.setContentTitle("Time's up!")
+
+        // Show the notification
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    fun setPreferences(){
+        val preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE)
+        val editor = preferences.edit()
+        Log.e("HourglassService", "setPreferences called with $remainingTime")
+        editor.putLong("remainingTime", remainingTime)
+        editor.putBoolean("isFirstPortraitCall", isFirstPortraitCall)
+        editor.putBoolean("isFirstReverseCall", isFirstReverseCall)
+        editor.apply()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        //Application has been closed
+        setPreferences()
+        stopTimer()
+        return super.onUnbind(intent)
+    }
+}
---
+++ app/src/main/java/com/timesup/MainActivity.kt
@@ -0,0 +1,257 @@
+package com.timesup
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.Surface
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.*
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.content.Intent
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    // Timer-related variables and constants
+    private lateinit var countDownTimer: CountDownTimer
+    private var remainingTime : Long = TIMER_DURATION
+
+    // Sensor-related variables
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var rotation: Int? = 0
+    private var isFirstPortraitCall = false
+    private var isFirstReverseCall = true
+
+    // UI elements
+    private lateinit var imgTopProgressBar : View
+    private lateinit var imgBottomProgressBar : View
+    private lateinit var imgTopSand : ImageView
+    private lateinit var imgBottomSand: ImageView
+    private lateinit var imgBlackMargin : ImageView
+    private lateinit var btnPlay : Button
+    private lateinit var tvTime : TextView
+
+    //Progress bar-related variables
+
+
+    //Service-related variables
+    private lateinit var hourglassService: HourglassBoundService
+    private var isBound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as HourglassBoundService.HourglassBinder
+            hourglassService = binder.service
+            isBound = true
+
+            //Retrieve timer parameters from the service
+            hourglassService.setPreferences()
+            hourglassService.stopTimer()
+            getPreferences()
+
+            if (rotation == Surface.ROTATION_180) {
+                if (isFirstReverseCall) {
+                    remainingTime = TIMER_DURATION - remainingTime
+                    isFirstReverseCall = false
+                    isFirstPortraitCall = true
+                }
+            } else if (rotation == Surface.ROTATION_0) {
+                if (isFirstPortraitCall) {
+                    remainingTime = TIMER_DURATION - remainingTime
+                    isFirstPortraitCall = false
+                    isFirstReverseCall = true
+                }
+            }
+
+            // Create the notification channel
+            hourglassService.createNotificationChannel()
+
+            startCountdownTimer()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("MainActivity", "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize sensor manager and accelerometer sensor
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // Initialize UI elements
+        imgTopProgressBar = findViewById(R.id.topProgressBar)
+        imgBottomProgressBar = findViewById(R.id.bottomProgressBar)
+        imgBottomSand = findViewById(R.id.img_bottomSand)
+        imgTopSand = findViewById(R.id.img_topSand)
+        imgBlackMargin = findViewById(R.id.img_blackMargin)
+        btnPlay = findViewById(R.id.btn_play)
+        tvTime = findViewById(R.id.tv_time)
+
+        // Determine the screen rotation
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val display = display
+            rotation = display?.rotation
+        } else {
+            rotation = windowManager.defaultDisplay.rotation
+        }
+
+        //Connection to the service
+        val intent = Intent(this, HourglassBoundService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
+        // Button click listener to easily manipulate the hourglass
+        btnPlay.setOnClickListener {
+            when (btnPlay.text) {
+                "Play" -> {
+                    startCountdownTimer()
+                }
+                "Pause" -> {
+                    countDownTimer.cancel()
+                    btnPlay.text = getString(R.string.restartCmd)
+                }
+                else -> {
+                    tvTime.text = getString(R.string.fullTime)
+                    remainingTime = TIMER_DURATION
+                    btnPlay.text = getString(R.string.playCmd)
+                    updateProgress(0f)
+                }
+            }
+        }
+    }
+
+    private fun getPreferences(){
+        val preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE)
+        remainingTime = preferences.getLong("remainingTime", TIMER_DURATION)
+        isFirstPortraitCall = preferences.getBoolean("isFirstPortraitCall", false)
+        isFirstReverseCall = preferences.getBoolean("isFirstReverseCall", true)
+    }
+
+    // Start the countdown timer
+    private fun startCountdownTimer() {
+        countDownTimer = object : CountDownTimer(remainingTime, COUNT_DOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Update the TextView with the remaining time
+                remainingTime = millisUntilFinished
+                val minutes = remainingTime / 1000 / 60
+                val seconds = remainingTime / 1000 % 60
+                tvTime.text = String.format("%02d:%02d", minutes, seconds)
+                // Compute the percentage of the time passed
+                val percentage = 1-(remainingTime.toFloat() / (TIMER_DURATION))
+
+                updateProgress(percentage)
+            }
+
+            override fun onFinish() {
+                remainingTime = TIMER_DURATION
+                tvTime.text = getString(R.string.fullTime)
+                btnPlay.text = getString(R.string.restartCmd)
+
+                // Create the notification
+                hourglassService.buildNotification()
+            }
+        }.start()
+        btnPlay.text = getString(R.string.pauseCmd)
+    }
+
+    // Update the progress bar based on the given percentage
+    private fun updateProgress(percentage: Float) {
+        val topLayoutParams = imgTopProgressBar.layoutParams as ViewGroup.MarginLayoutParams
+        val bottomLayoutParams = imgBottomProgressBar.layoutParams as ViewGroup.MarginLayoutParams
+
+        // Calculate margins to adapt the progress bars to the layout
+        topLayoutParams.topMargin = imgBlackMargin.height/10
+        bottomLayoutParams.topMargin = imgBlackMargin.height*21/40
+
+        // Calculate heights and widths for the progress bars
+        imgTopProgressBar.layoutParams.height = (percentage * (imgBlackMargin.height*3/8)).toInt()
+        imgTopProgressBar.layoutParams.width = imgBlackMargin.width
+        imgTopProgressBar.requestLayout()
+
+        imgBottomProgressBar.layoutParams.height = ((imgBlackMargin.height*3/8) - (percentage*(imgBlackMargin.height*3/8))).toInt()
+        imgBottomProgressBar.layoutParams.width = imgBlackMargin.width
+        imgBottomProgressBar.requestLayout()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+
+        if(isBound) {
+            hourglassService.setRemainingTime(remainingTime)
+            hourglassService.setFirstPortraitCall(isFirstPortraitCall)
+            hourglassService.setFirstReverseCall(isFirstReverseCall)
+            hourglassService.startTimer()
+            countDownTimer.cancel()
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            // Get the accelerometer values
+            val x = event.values[0]
+            val y = event.values[1]
+
+            // Rotate UI elements based on screen rotation
+            when (rotation) {
+                Surface.ROTATION_0 -> {
+                    imgTopProgressBar.rotation = if (y < 0) -x else x
+                    imgBottomProgressBar.rotation = if (y < 0) -x else x
+                    imgBottomSand.rotation = if (y < 0) -x else x
+                    imgTopSand.rotation = if (y < 0) -x else x
+                    imgBlackMargin.rotation = if (y < 0) -x else x
+                }
+                Surface.ROTATION_90 -> {
+                    imgTopProgressBar.rotation = if (x > 0) -y else y
+                    imgBottomProgressBar.rotation = if (x > 0) -y else y
+                    imgBottomSand.rotation = if (x > 0) -y else y
+                    imgTopSand.rotation = if (x > 0) -y else y
+                    imgBlackMargin.rotation = if (x > 0) -y else y
+                }
+                Surface.ROTATION_180 -> {
+                    imgTopSand.rotation = if (y > 0) x else -x
+                    imgBottomSand.rotation = if (y > 0) x else -x
+                    imgTopProgressBar.rotation = if (y > 0) x + 180 else -x + 180
+                    imgBottomProgressBar.rotation = if (y > 0) x + 180 else -x + 180
+                    imgBlackMargin.rotation = if (y > 0) x + 180 else -x + 180
+                }
+                Surface.ROTATION_270 -> {
+                    imgTopProgressBar.rotation = if (x < 0) y else -y
+                    imgBottomProgressBar.rotation = if (x < 0) y else -y
+                    imgBottomSand.rotation = if (x < 0) y else -y
+                    imgTopSand.rotation = if (x < 0) y else -y
+                    imgBlackMargin.rotation = if (x < 0) y else -y
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("MainActivity", "onAccuracyChanged: $sensor, accuracy: $accuracy")
+    }
+
+    companion object {
+        const val TIMER_DURATION: Long = 10 * 60 * 1000
+        const val COUNT_DOWN_INTERVAL : Long = 1000
+    }
+}
